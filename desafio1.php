3130c96e1473dc448355ed56248644374dbe2280
yasmim <jasmimyaya56@gmail.com>
2026-08-13 01:31:33 +0000

    adicionado o desafio1.php

---
+++ desafio1.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+1º desafio, escrever essa frase com as variaveis que criamos:
+"meu nome é [seu nome] e tenho [sua idade] anos."
+*/
+
+$nome = "Yasmim"; //string (texto)
+$idade = 18;      //integer (inteiro)
+
+
+echo 'Meu nome é $nome e tenho $idade anos. <br><br>';
+
+echo "Meu nome é $nome e tenho $idade anos. <br><br>";
+
+echo "Meu nome é" . $nome . " e tenho" . $idade . " anos.";
+
+?>
+
