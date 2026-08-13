f5959fb8fa51db72822f4d2c6567a899f19a1156
yasmim <jasmimyaya56@gmail.com>
2026-08-13 01:30:48 +0000

    adicionado o index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$nome = "Yasmim"; //string (texto)
+$idade = 18;      //integer (inteiro)
+$peso = 75.5;     //float (decimal)
+$vivo = true;     //boolean (verdadeiro / falso) (true / false)
+
+//comentario de linha unica
+echo $nome . $idade . $peso . $vivo;
+
+/*
+1º desafio, escrever essa frase com as variaveis que criamos:
+"meu nome é [seu nome] e tenho [sua idade] anos."
+*/
+
+echo "meu nome é $nome e tenho $idade anos."
+
+
+
+?>
+https://onecompiler.com/php
